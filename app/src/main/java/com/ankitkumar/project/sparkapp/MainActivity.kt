2ad14c9da7b9bbc41072e27f727d15d66d7bff12
Ankit Kumar <ankitkr5447@gmail.com>
2025-04-05 14:01:42 +0530

    added opening page

--- app/src/main/java/com/ankitkumar/project/sparkapp/MainActivity.kt
+++ app/src/main/java/com/ankitkumar/project/sparkapp/MainActivity.kt
@@ -17,6 +17,7 @@ limitations under the License.
 
 package com.ankitkumar.project.sparkapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this,open_screen::class.java))
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/ankitkumar/project/sparkapp/Splash.kt
@@ -0,0 +1,24 @@
+package com.ankitkumar.project.sparkapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Splash : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+      //  setContentView(R.layout.activity_splash)
+        startActivity(Intent(this,MainActivity::class.java))
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
